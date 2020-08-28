5a6e9e11861b906948120fe6f20238156d861087
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-28 00:34:05 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,3 +23,21 @@ def get_english_meaning(yaml_file, emoticon)
   english_meaning = emo_conv.keys.find { |key| emo_conv[key][:japanese] == emoticon }
   english_meaning ? english_meaning : "Sorry, that emoticon was not found"
 end
+
+def get_japanese_emoticon(yaml_file, emoticon)
+  load_library(yaml_file).each do |key, value|
+    if value[:english] == emoticon
+      return value[:japanese]
+    end
+  end
+  return "Sorry, that emoticon was not found"
+end
+
+def get_english_meaning(yaml_file, emoticon)
+  load_library(yaml_file).each do |key, value|
+    if value[:japanese] == emoticon
+      return key
+    end
+  end
+  return "Sorry, that emoticon was not found"
+end
